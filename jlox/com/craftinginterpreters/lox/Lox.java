495b58362cef7cda6affa51297f22abf5c3a63e3
Baozhe Zhang <907711755@qq.com>
2023-08-22 14:06:49 +0800

    Finished chapter 6
    Learned about recursive descent parsing (RDS) to parse expression by
    using java method calls.

--- jlox/com/craftinginterpreters/lox/Lox.java
+++ jlox/com/craftinginterpreters/lox/Lox.java
@@ -46,20 +46,32 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source); 
     List<Token> tokens = scanner.scanTokens(); 
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-    for (Token token : tokens) {
-      System.out.println(token); 
-    }
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
+
   }
 
   static void error(int line, String message) {
     report(line, "", message); 
   }
 
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
+
   private static void report(int line, String where, 
                              String message) {
     
-    System.err.println("[line ]" + line + "] Error" + where + ": " + message);
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
   }
+
 }
