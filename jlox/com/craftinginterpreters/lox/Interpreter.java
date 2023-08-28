ff29c56ad08092ae6d14f831e4d0fc080ac4b16c
Baozhe Zhang <907711755@qq.com>
2023-08-28 14:48:41 +0800

    enclosing

--- jlox/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/com/craftinginterpreters/lox/Interpreter.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, 
                              Stmt.Visitor<Void> {
+  private Environment environment = new Environment();
 
   void interpret(List<Stmt> statements) {
     try {
@@ -33,6 +34,11 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     return null;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
   }
 
   private void checkNumberOperand(Token operator, Object operand) {
@@ -98,6 +104,24 @@ class Interpreter implements Expr.Visitor<Object>,
     Object value = evaluate(stmt.expression);
     System.out.println(stringfy(value));
     return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, expr.value);
+    return value;
   }
 
   @Override
--- jlox/com/craftinginterpreters/lox/Parser.java
+++ jlox/com/craftinginterpreters/lox/Parser.java
@@ -18,14 +18,25 @@ class Parser {
   List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>(); 
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
 
     return statements;
   }
 
   private Expr expression() {
-    return equality(); 
+    return assignment(); 
+  }
+
+  private Stmt declaration() {
+    try {
+      if (match(VAR)) return varDeclaration();
+
+      return statement(); 
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
   }
 
   private Stmt statement() {
@@ -40,10 +51,40 @@ class Parser {
     return new Stmt.Print(value);
   }
 
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name.");
+
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression(); 
+    }
+
+    consume(SEMICOLON, "Expect ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
+  }
+
   private Stmt expressionStatement() {
     Expr expr = expression();
     consume(SEMICOLON, "Expect ';' after expression.");
     return new Stmt.Expression(expr);
+  }
+
+  private Expr assignment() {
+    Expr expr = equality(); 
+
+    if (match(EQUAL)) {
+      Token equals = previous();
+      Expr value = assignment(); 
+
+      if (expr instanceof Expr.Variable) {
+        Token name = ((Expr.Variable)expr).name;
+        return new Expr.Assign(name, value);
+      }
+
+      error(equals, "Invalid assignment target.");
+    }
+
+    return expr;
   }
 
   private Expr equality() {
@@ -111,6 +152,10 @@ class Parser {
 
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal); 
+    }
+    
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
 
     if (match(LEFT_PAREN)) {
